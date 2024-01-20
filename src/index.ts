44d5082573b9557bedd1fdad84f58bf91bca9c28
Moaz Ashraf <moaazashraf98@gmail.com>
2024-01-20 15:48:05 +0200

    adding new data

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import baseRoutes from "../Routes/routes";
+import cors from "cors";
 
 dotenv.config();
 
@@ -10,6 +11,14 @@ const port = process.env.PORT || 3000;
 
 // Middleware setup for JSON parsing
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 // Middleware setup for "/nawy/apartments"
 app.use("/nawy/apartments", baseRoutes);
 
